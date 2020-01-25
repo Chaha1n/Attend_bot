ec5f5b5dc56d59e1e78a7f676af6db890d7dc6d7
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-25 14:11:16 +0900

    トークンやっぱ生で書く

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var twitter = require('twitter');
 const fs = require('fs');
-var twitter = new twitter(
-    JSON.parse(fs.readFileSync("Token.json", "utf-8"))
-);
+tokens = fs.readFileSync("Token.json");
+var twitter = new twitter({
+    consumer_key: "kfiLkug9OvQIYdzv5FtS0Fgtq",
+    consumer_secret: "3yr8ebzg0XzeNrykTy2Mgfb3sqtwER1abGmPTu1vYMnU317YR6",
+    access_token_key: "1218145243440140288-UrQlS3DPQfOJyXmaiR2PyMo2MhQy2C",
+    access_token_secret: "HbbeyXdrDzK8DSw9Hza2JAoul7SZ0WiTF4CcZzDB7mLJa"
+});
 //メンションされた時の処理
 twitter.stream('statuses/filter', { track: "@Attend_bot" }, function (stream) {
     stream.on('data', function (data) {
@@ -23,3 +27,5 @@ twitter.stream('statuses/filter', { track: "@Attend_bot" }, function (stream) {
         })
     })
 })
+//自動でフォローを返す
+
