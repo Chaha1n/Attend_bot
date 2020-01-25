cc54c197fa6f3f20c281abc3304120abe3eb8bf7
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-25 13:19:59 +0900

    リポジトリの作成に伴い現状のファイルをアップロード

---
+++ server.js
@@ -0,0 +1,26 @@
+var twitter = require('twitter');
+var twitter = new twitter({
+    consumer_key: "kfiLkug9OvQIYdzv5FtS0Fgtq",
+    consumer_secret: "3yr8ebzg0XzeNrykTy2Mgfb3sqtwER1abGmPTu1vYMnU317YR6",
+    access_token_key: "1218145243440140288-UrQlS3DPQfOJyXmaiR2PyMo2MhQy2C",
+    access_token_secret: "HbbeyXdrDzK8DSw9Hza2JAoul7SZ0WiTF4CcZzDB7mLJa"
+});
+twitter.stream('statuses/filter', { track: "@Attend_bot" }, function (stream) {
+    stream.on('data', function (data) {
+        var tweet = data.text;
+        console.log(tweet);
+        console.log(data.id_str);
+        //ツイートをする
+        twitter.post('statuses/update', {
+            status: "Hey @" + data.user.screen_name + ", Thank you for your mention.",
+            in_reply_to_status_id:data.id_str
+        }, (err, tweet, response) => {
+            if (err) {
+                return console.log(err)
+            }
+            else {
+                return console.log(tweet)
+            }
+        })
+    })
+})
