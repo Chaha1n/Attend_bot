7897b225b75764c40dad5118e4a9ff708dfbe343
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-25 13:58:58 +0900

    トークンをjsonから読み込むことにしました

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var twitter = require('twitter');
-var twitter = new twitter({
-    consumer_key: "kfiLkug9OvQIYdzv5FtS0Fgtq",
-    consumer_secret: "3yr8ebzg0XzeNrykTy2Mgfb3sqtwER1abGmPTu1vYMnU317YR6",
-    access_token_key: "1218145243440140288-UrQlS3DPQfOJyXmaiR2PyMo2MhQy2C",
-    access_token_secret: "HbbeyXdrDzK8DSw9Hza2JAoul7SZ0WiTF4CcZzDB7mLJa"
-});
+const fs = require('fs');
+var twitter = new twitter(
+    JSON.parse(fs.readFileSync("Token.json", "utf-8"))
+);
 //メンションされた時の処理
 twitter.stream('statuses/filter', { track: "@Attend_bot" }, function (stream) {
     stream.on('data', function (data) {
