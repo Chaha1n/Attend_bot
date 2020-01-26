06cb26e9c71c1c9643624e1b5515fb0e3a4fb6b7
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-26 17:11:41 +0900

    Twitterオブジェクトをモジュールから呼ぶようにした

---
+++ followers.js
@@ -0,0 +1,33 @@
+var twitter = require('./Twitter.js')();
+var fs = require('fs');
+module.exports.GetList = function(twitter,params){
+    var fs = require('fs');
+    twitter.get('followers/list', params,
+        function (error, followers, responce) {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                fs.writeFileSync("followers.json", JSON.stringify(followers) + "\n", "utf-8");
+                console.log(responce);
+            }
+        }
+    );
+}
+
+var followers = JSON.parse(fs.readFileSync("followers.json", "utf-8"));
+console.log(followers["users"][0]["screen_name"]);
+for (var i = 0; i < Object.keys(followers["users"]).length; i++) {
+    var target = followers["users"][i].screen_name;
+    twitter.post('friendships/create',{screen_name:target},
+        (error, friendship, responce) => {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                console.log(friendship);
+                console.log(responce);
+            }
+        }
+    );
+}
