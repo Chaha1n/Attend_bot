0846bed21dad9427f75b26ff9b223be1818cf89c
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-26 17:10:37 +0900

    Twitterオブジェクトをモジュールから呼ぶようにした

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-var twitter = require('twitter');
+var twitter = require('./Twitter.js')();
 const fs = require('fs');
-var twitter = new twitter({
-    consumer_key: "kfiLkug9OvQIYdzv5FtS0Fgtq",
-    consumer_secret: "3yr8ebzg0XzeNrykTy2Mgfb3sqtwER1abGmPTu1vYMnU317YR6",
-    access_token_key: "1218145243440140288-UrQlS3DPQfOJyXmaiR2PyMo2MhQy2C",
-    access_token_secret: "HbbeyXdrDzK8DSw9Hza2JAoul7SZ0WiTF4CcZzDB7mLJa"
-});
+
 //メンションされた時の処理
 twitter.stream('statuses/filter', { track: "@Attend_bot" }, function (stream) {
     stream.on('data', function (data) {
@@ -29,17 +24,7 @@ twitter.stream('statuses/filter', { track: "@Attend_bot" }, function (stream) {
 //自動でフォローを返す
 const params = { screen_name: 'Attend_bot', count: 50 };
 console.log('@' + params.screen_name);
-twitter.get('followers/list', params,
-    function (error, followers, responce) {
-        if (error) {
-            console.log(error);
-        }
-        else {
-            fs.writeFileSync("followers.json", JSON.stringify(followers) + "\n", "utf-8");
-            console.log(responce);
-        }
-    }
-);
+require('./Before.js')(twitter,params);
 
 var followers = JSON.parse(fs.readFileSync("followers.json", "utf-8"));
 console.log(followers["users"][0]["screen_name"]);
