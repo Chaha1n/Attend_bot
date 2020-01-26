8a925467eea9f179a764694c4257f2135ff5440f
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-26 17:18:12 +0900

    フォローバックをモジュール化

--- followers.js
+++ followers.js
@@ -15,19 +15,21 @@ module.exports.GetList = function(twitter,params){
     );
 }
 
-var followers = JSON.parse(fs.readFileSync("followers.json", "utf-8"));
-console.log(followers["users"][0]["screen_name"]);
-for (var i = 0; i < Object.keys(followers["users"]).length; i++) {
-    var target = followers["users"][i].screen_name;
-    twitter.post('friendships/create',{screen_name:target},
-        (error, friendship, responce) => {
-            if (error) {
-                console.log(error);
+module.exports.AutoFollow = function (twitter) {
+    var followers = JSON.parse(fs.readFileSync("followers.json", "utf-8"));
+    console.log(followers["users"][0]["screen_name"]);
+    for (var i = 0; i < Object.keys(followers["users"]).length; i++) {
+        var target = followers["users"][i].screen_name;
+        twitter.post('friendships/create', { screen_name: target },
+            (error, friendship, responce) => {
+                if (error) {
+                    console.log(error);
+                }
+                else {
+                    console.log(friendship);
+                    console.log(responce);
+                }
             }
-            else {
-                console.log(friendship);
-                console.log(responce);
-            }
-        }
-    );
+        );
+    }
 }
