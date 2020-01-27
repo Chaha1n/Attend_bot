06ce28b16b7e9dd965b0e6ef62e5d830b0ce7dd9
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-27 17:51:45 +0900

    コメントの軽微な変更

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 //メンションされた時の処理
 require("./Mention.js")(twitter);
-//フォロワーリストの取得
+//フォロワーリストを取得しFollowers.jsonに書き込み
 const params = { screen_name: 'Attend_bot', count: 50 };
 require('./GetList.js')(twitter,params);
 
