21b26820213134012a841dbafcb9fc3d20b3b041
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-27 22:17:17 +0900

    定期ツイートの時刻を変更

--- Force.js
+++ Force.js
@@ -1,17 +1,18 @@
 module.exports = function (twitter) {
     var date = new Date();
     var CronJob = require('cron').CronJob;
-    const PostTime = '0 0 7-9 * * 1-5';
+    const PostTime = '0 /10  6-9 * 1-5';
 
     var month = date.getMonth() + 1;
     var day = date.getDate();
-    var hour = date.getUTCHours + 9;
+    var hour = date.getUTCHours() + 9;
+    var minute = date.getUTCMinutes();
 
     new CronJob({
         cronTime: PostTime,
         onTick: function () {
             twitter.post('statuses/update', {
-                status: month+"月 "+day+"日 "+hour+"時 00 分です。出席してください。"
+                status: month+"月 "+day+"日 "+hour+"時"+minute+"分です。出席しませんか？"
             }, (err, tweet, response) => {
                 if (err) {
                     return console.log(err);
