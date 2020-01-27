66c15f8a8f6af50d23a0b06b8ad2774bb22e120e
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-27 21:49:21 +0900

    部活の時間にツイートするようにした

--- Force.js
+++ Force.js
@@ -1,9 +1,23 @@
-module.exports = function (twitter, message) {
-    twitter.post('statuses/update', {
-        status: message + "です。出席しましょう。"
-    }, (err, tweet, response) => {
-        if (err) {
-            return console.log(err);
-        }
+module.exports = function (twitter) {
+    var date = new Date();
+    var cronJob = require('cron').CronJob;
+    const PostTime = '0 0 7-9 * * 1-5';
+
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getUTCHours + 9;
+
+    new CronJob({
+        cronTime: PostTime,
+        onTick: function () {
+            twitter.post('statuses/update', {
+                status: month+"月 "+day+"日 "+hour+"時 00 分です。出席してください。"
+            }, (err, tweet, response) => {
+                if (err) {
+                    return console.log(err);
+                }
+            })
+        },
+        start:true
     })
 }
