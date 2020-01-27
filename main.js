a0a2f67b017e952dbbb359e2a7143f4c78638c8e
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-27 22:08:17 +0900

    定時ツイートを呼ぶようにしました

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var twitter = require('./Twitter.js')();
-var Cronjob = require("cron").CronJob
-const FollowTime = '0 0 * * *'
+var CronJob = require("cron").CronJob
+const FollowTime = '0 0 * * * *'
 const fs = require('fs');
 
 //メンションされた時の処理
@@ -10,8 +10,10 @@ const params = { screen_name: 'Attend_bot', count: 50 };
 require('./GetList.js')(twitter,params);
 
 //毎日毎時ちょうどに自動でフォローバック
-new Cronjob({
+new CronJob({
     cronTime:FollowTime,
     onTick: require('./followers.js').AutoFollow(twitter),
     start:true
 })
+//平日16,17,18時に出席呼びかけ
+require('./Force.js')(twitter);
