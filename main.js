f5afc990ed75d68ae24818297b308913d63f70a0
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-27 17:38:16 +0900

    メンションをモジュール化

--- main.js
+++ main.js
@@ -2,25 +2,7 @@ var twitter = require('./Twitter.js')();
 const fs = require('fs');
 
 //メンションされた時の処理
-twitter.stream('statuses/filter', { track: "@Attend_bot" }, function (stream) {
-    stream.on('data', function (data) {
-        var tweet = data.text;
-        console.log(tweet);
-        console.log(data.id_str);
-        //ツイートをする
-        twitter.post('statuses/update', {
-            status: "Hey @" + data.user.screen_name + ", Thank you for your mention.",
-            in_reply_to_status_id: data.id_str
-        }, (err, tweet, response) => {
-            if (err) {
-                return console.log(err);
-            }
-            else {
-                return console.log("From @"+data.user.screen_name,tweet);
-            }
-        })
-    })
-})
+require("./Mention.js")(twitter);
 //フォロワーリストの取得
 const params = { screen_name: 'Attend_bot', count: 50 };
 require('./GetList.js')(twitter,params);
