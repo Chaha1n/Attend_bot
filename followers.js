7b7ce6de1f466907abcd732dcf113c5c838a81a4
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-30 17:03:42 +0900

    Cron部分を外に出した

--- followers.js
+++ followers.js
@@ -1,27 +1,19 @@
 module.exports =
     function (twitter) {
         var fs = require('fs');
-        var CronJob = require("cron").CronJob
-        const FollowTime = '0 0 * * * *'
-        new CronJob({
-            cronTime: FollowTime,
-            onTick: function (twitter) {
-                var followers = JSON.parse(fs.readFileSync("followers.json", "utf-8"));
-                console.log(followers["users"][0]["screen_name"]);
-                for (var i = 0; i < Object.keys(followers["users"]).length; i++) {
-                    var target = followers["users"][i].screen_name;
-                    twitter.post('friendships/create', { screen_name: target },
-                        (error, friendship, responce) => {
-                            if (error) {
-                                console.log(error);
-                            }
-                            else {
-                                console.log(friendship);
-                            }
-                        }
-                    );
+        var followers = JSON.parse(fs.readFileSync("followers.json", "utf-8"));
+        console.log(followers["users"][0]["screen_name"]);
+        for (var i = 0; i < Object.keys(followers["users"]).length; i++) {
+            var target = followers["users"][i].screen_name;
+            twitter.post('friendships/create', { screen_name: target },
+                (error, friendship, responce) => {
+                    if (error) {
+                        console.log(error);
+                    }
+                    else {
+                        console.log(friendship);
+                    }
                 }
-            },
-            start: true
-        })
+            );
+        }
     }
