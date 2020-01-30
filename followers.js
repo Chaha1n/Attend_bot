2679347888682c5a3e44c9bd682226f3b4794814
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-30 16:54:24 +0900

    ごめんなんの変更かよくわからん

--- followers.js
+++ followers.js
@@ -1,20 +1,27 @@
-var twitter = require('./Twitter.js')();
-var fs = require('fs');
-
-module.exports.AutoFollow = function (twitter) {
-    var followers = JSON.parse(fs.readFileSync("followers.json", "utf-8"));
-    console.log(followers["users"][0]["screen_name"]);
-    for (var i = 0; i < Object.keys(followers["users"]).length; i++) {
-        var target = followers["users"][i].screen_name;
-        twitter.post('friendships/create', { screen_name: target },
-            (error, friendship, responce) => {
-                if (error) {
-                    console.log(error);
+module.exports =
+    function (twitter) {
+        var fs = require('fs');
+        var CronJob = require("cron").CronJob
+        const FollowTime = '0 0 * * * *'
+        new CronJob({
+            cronTime: FollowTime,
+            onTick: function (twitter) {
+                var followers = JSON.parse(fs.readFileSync("followers.json", "utf-8"));
+                console.log(followers["users"][0]["screen_name"]);
+                for (var i = 0; i < Object.keys(followers["users"]).length; i++) {
+                    var target = followers["users"][i].screen_name;
+                    twitter.post('friendships/create', { screen_name: target },
+                        (error, friendship, responce) => {
+                            if (error) {
+                                console.log(error);
+                            }
+                            else {
+                                console.log(friendship);
+                            }
+                        }
+                    );
                 }
-                else {
-                    console.log(friendship);
-                }
-            }
-        );
+            },
+            start: true
+        })
     }
-}
