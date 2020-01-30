b74cd982925b545861f51e1fc6a4c355f414c658
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-30 17:03:18 +0900

    Cron部分をmainに出した

--- Force.js
+++ Force.js
@@ -1,24 +1,16 @@
 module.exports = function (twitter) {
     var date = new Date();
     var CronJob = require('cron').CronJob;
-    const PostTime = '0 */10 6-9 * * 1-5';
     var month = date.getMonth() + 1;
     var day = date.getDate();
     var hour = date.getUTCHours() + 9;
     var minute = date.getUTCMinutes();
 
-
-    new CronJob({
-        cronTime: PostTime,
-        onTick: function () {
-            twitter.post('statuses/update', {
-                status: month + "月" + day + "日、ただいまの時刻は" + hour + "時" + minute + "分です。 コンピュータ部のみなさん、出席してみませんか？"
-            }, (err, tweet, response) => {
-                if (err) {
-                    return console.log(err);
-                }
-            })
-        },
-        start: true
+    twitter.post('statuses/update', {
+        status: month + "月" + day + "日、ただいまの時刻は" + hour + "時" + minute + "分です。 コンピュータ部のみなさん、出席してみませんか？"
+    }, (err, tweet, response) => {
+        if (err) {
+            return console.log(err);
+        }
     })
 }
