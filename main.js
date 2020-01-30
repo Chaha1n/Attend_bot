4131a68459e88cbaba8a0a18a7a6dcbcb8223356
chahaaaaan <Chahaaaaam@gmail.com>
2020-01-30 16:55:57 +0900

    Force.jsを実装

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 var twitter = require('./Twitter.js')();
-var CronJob = require("cron").CronJob
-const FollowTime = '0 0 * * * *'
 const fs = require('fs');
 
 //メンションされた時の処理
@@ -10,10 +8,6 @@ const params = { screen_name: 'Attend_bot', count: 50 };
 require('./GetList.js')(twitter,params);
 
 //毎日毎時ちょうどに自動でフォローバック
-new CronJob({
-    cronTime:FollowTime,
-    onTick: require('./followers.js').AutoFollow(twitter),
-    start:true
-})
+require('./followers.js')(twitter);
 //平日15～18:50まで10分おきに出席呼びかけ
 require('./Force.js')(twitter);
