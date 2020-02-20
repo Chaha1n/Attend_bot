2223c2bb24dcd03a86ca1ed76473564d18e9661b
chahaaaaan <Chahaaaaam@gmail.com>
2020-02-20 09:41:57 +0900

    Expressでのポートリッスンを追加

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var twitter = require('./Twitter.js')();
 const fs = require('fs');
+var express = require('express')
+var app = express()
 
 //メンションされた時の処理
 require("./Mention.js")(twitter);
@@ -21,4 +23,11 @@ new CronJob({
     cronTime: PostTime,
     onTick: require('./Call.js')(twitter),
     start: true
+})
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function (req, res) {
+    res.send("Hello World");
+})
+app.listen(app.get('port'), function (){
+    console.log("Node app is runnning at localhost:"+app.get('port'))
 })
