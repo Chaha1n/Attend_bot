b47cc5ca9155be67a0feea0a4f389f7a5e892dee
chahaaaaan <Chahaaaaam@gmail.com>
2020-02-20 09:36:49 +0900

    トークンを環境変数に置き換え

--- Twitter.js
+++ Twitter.js
@@ -2,10 +2,10 @@ module.exports = function () {
     var twitter = require('twitter');
     var fs = require('fs');
     var twitter = new twitter({
-        consumer_key: "kfiLkug9OvQIYdzv5FtS0Fgtq",
-        consumer_secret: "3yr8ebzg0XzeNrykTy2Mgfb3sqtwER1abGmPTu1vYMnU317YR6",
-        access_token_key: "1218145243440140288-UrQlS3DPQfOJyXmaiR2PyMo2MhQy2C",
-        access_token_secret: "HbbeyXdrDzK8DSw9Hza2JAoul7SZ0WiTF4CcZzDB7mLJa"
+        consumer_key: process.env['CONSUMER_KEY'],
+        consumer_secret: process.env['CONSUMER_SECRET_KEY'],
+        access_token_key: process.env['ACCESS_TOKEN_KEY'],
+        access_token_secret: process.env['ACCESS_TOKEN_SECRET']
     });
     return twitter;
 }
